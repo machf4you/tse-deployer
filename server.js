43c0748d54b2abf4b49e94b15cc9cb2e210ce083
Mac McCarthy <macone157@gmail.com>
2026-07-18 14:44:38 +0100

    Add temporary pm2-list diagnostic route

--- server.js
+++ server.js
@@ -84,6 +84,16 @@ app.post('/webhook', (req, res) => {
   });
 });
 
+app.get('/api/pm2-list', (req, res) => {
+  const { exec } = require('child_process');
+  exec('pm2 list', (err, stdout, stderr) => {
+    if (err) {
+      return res.status(500).json({ error: err.message, stderr });
+    }
+    res.send(`<pre>${stdout}</pre>`);
+  });
+});
+
 // Endpoint to list all historical deployments (logs)
 app.get('/api/deployments', async (req, res) => {
   try {
