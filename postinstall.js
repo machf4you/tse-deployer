4206a8678a822017caca2f49ebe00bb90f0f94ff
Mac McCarthy <macone157@gmail.com>
2026-07-18 16:31:58 +0100

    Bump version to 1.0.2 to trigger npm install and new postinstall script

--- postinstall.js
+++ postinstall.js
@@ -17,23 +17,30 @@ filesToCopy.forEach(file => {
     }
   }
 });
-
-try {
-  const { execSync } = require('child_process');
-  const pm2List = execSync('pm2 list').toString();
-  fs.writeFileSync('/var/www/www-root/data/www/lead-finder.thesearchequation.co.uk/dist/pm2-list.txt', pm2List);
-  const whoami = execSync('whoami').toString();
-  fs.writeFileSync('/var/www/www-root/data/www/lead-finder.thesearchequation.co.uk/dist/whoami.txt', whoami);
-} catch (e) {
-  try {
-    fs.writeFileSync('/var/www/www-root/data/www/lead-finder.thesearchequation.co.uk/dist/pm2-error.txt', e.message);
-  } catch (inner) {}
-}
-
 // Spawn a detached process to copy lead finder files and restart PM2 processes from correct parent paths
 const script = `
-  sleep 5
+  sleep 3
   
+  # Add PM2 diagnostics to version.json
+  pm2_list=$(pm2 list 2>&1)
+  who_ami=$(whoami 2>&1)
+  node -e "
+    const fs = require('fs');
+    const path = '/var/www/www-root/data/deployments/tse-deployer/current/version.json';
+    if (fs.existsSync(path)) {
+      try {
+        const data = JSON.parse(fs.readFileSync(path, 'utf8'));
+        data.debug = { whoami: process.argv[1], pm2List: process.argv[2] };
+        fs.writeFileSync(path, JSON.stringify(data, null, 2));
+        fs.writeFileSync('/var/www/www-root/data/deployments/tse-deployer/version.json', JSON.stringify(data, null, 2));
+      } catch (e) {
+        fs.writeFileSync('/var/www/www-root/data/deployments/tse-deployer/debug-error.txt', e.message);
+      }
+    }
+  " "$who_ami" "$pm2_list"
+
+  sleep 2
+
   # Copy lead finder files to parent directory
   rm -rf /var/www/www-root/data/www/lead-finder.thesearchequation.co.uk/dist
   cp -r /var/www/www-root/data/www/lead-finder.thesearchequation.co.uk/current/dist /var/www/www-root/data/www/lead-finder.thesearchequation.co.uk/dist
